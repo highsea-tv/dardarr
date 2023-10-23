471976ef5fea6ac7826e969abfd0f7771dd6fcad
Sebastian Inman <hello@sebastianinman.me>
2023-10-23 11:04:23 -0700

    auto generate theme imports. added theme extension type module

--- packages/types/extensions.d.ts
+++ packages/types/extensions.d.ts
@@ -7,3 +7,8 @@ declare module '@packages/locales/*.json' {
     const content: Schema.Internationalization
     export default content
 }
+
+declare module '@packages/themes/*.json' {
+    const content: Schema.Theme
+    export default content
+}
